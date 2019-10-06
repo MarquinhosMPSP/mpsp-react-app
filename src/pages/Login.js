3c31f3cedee64aefb3c6abb3e11adfba2eb96137
Daniel <marquinhosmpsp@outlook.com>
2019-10-06 20:38:42 -0300

    Alterando login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,116 +1,164 @@
-import React, { useState, useEffect } from 'react'
-import { View, AsyncStorage, KeyboardAvoidingView, StyleSheet, TextInput, TouchableOpacity, Text, Image } from 'react-native'
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  AsyncStorage,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Image,
+  ActivityIndicator,
+  ImageBackground,
+  Alert
+} from "react-native";
 
-import logo from '../assets/unikor.png'
+import logo from "../assets/unikor-white.png";
+import banner from "../assets/banner-min.png";
 
-import api from '../services/api'
+import api from "../services/api";
 
 export default function Login({ navigation }) {
+  const [cpf, setCpf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
-    const [cpf, setCpf] = useState('')
-    const [senha, setSenha] = useState('')
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user => {
+      if (user) {
+        navigation.navigate("List");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        AsyncStorage.getItem('user').then(user => {
-            if(user){
-                navigation.navigate('List')
-            }
-        })
-    }, [])
+  useEffect(() => {
+    setDisabled(!(cpf.length > 0 && senha.length > 0));
+  }, [cpf, senha]);
 
-    async function handleSubmit() {
-        const response = await api.post('/autenticar', { cpf, senha })        
+  async function handleSubmit() {
+    setLoading(true);
+    api
+      .post("/autenticar", { cpf, senha })
+      .then(async response => {
+        const { _id, nome } = response.data;
+        await AsyncStorage.setItem("user", _id);
+        await AsyncStorage.setItem("nome", nome);
+        setLoading(false);
+        navigation.navigate("List");
+      })
+      .catch(err => {
+        if (err.response.status == "404") {
+          setLoading(false);
+          Alert.alert("Autenticação", err.response.data.message);
+        }
+      });
+  }
 
-        const { _id, nome } = response.data
-
-        await AsyncStorage.setItem('user', _id)        
-        await AsyncStorage.setItem('nome', nome) 
-        
-        navigation.navigate('List')
-    }
-
-    return <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <View style={styles.form}>
+  return (
+    <ImageBackground source={banner} style={styles.background}>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        {loading ? (
+          <ActivityIndicator animating={true} size="large" color="#fff" />
+        ) : (
+          <View style={styles.form}>
             <Image style={styles.logo} source={logo} />
-            
+
             <Text style={styles.subTitle}>Relatórios integrados</Text>
 
             <Text style={styles.label}>SEU CPF *</Text>
             <TextInput
-                style={styles.input}
-                placeholder="Seu CPF"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={cpf}
-                onChangeText={setCpf}
-                autoFocus={true}
+              style={styles.input}
+              placeholder="Seu CPF"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={cpf}
+              onChangeText={setCpf}
+              autoFocus={true}
             />
 
             <Text style={styles.label}>SUA SENHA *</Text>
             <TextInput
-                style={styles.input}
-                placeholder="Sua senha"
-                autoCorrect={false}
-                value={senha}
-                onChangeText={setSenha}
-                autoCompleteType='password'
-                secureTextEntry={true}
+              style={styles.input}
+              placeholder="Sua senha"
+              autoCorrect={false}
+              value={senha}
+              onChangeText={setSenha}
+              autoCompleteType="password"
+              secureTextEntry={true}
             />
 
-            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-                <Text style={styles.buttonText}>Entrar</Text>
+            <TouchableOpacity
+              disabled={disabled}
+              onPress={handleSubmit}
+              style={disabled ? styles.disabled : styles.button}
+            >
+              <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
-        </View>
-    </KeyboardAvoidingView>
+          </View>
+        )}
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    label: {
-        fontWeight: 'bold',
-        color: '#444',
-        marginBottom: 8
-    },
-    form: {
-        alignSelf: 'stretch',
-        paddingHorizontal: 30,
-        marginBottom: 0
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ddd',
-        paddingHorizontal: 20,
-        fontSize: 16,
-        color: '#444',
-        marginBottom: 20,
-        borderRadius: 2
-    },
-    button: {
-        height: 42,
-        backgroundColor: '#DC3545',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 16
-    },
-    logo: {
-        height: 110,
-        resizeMode: "contain",
-        alignSelf: "center"
-    },
-    subTitle: {
-        fontWeight: "bold",
-        fontSize: 18,
-        alignSelf: "center",
-        marginBottom: 30
-    }
-})
+  background: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  label: {
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 8
+  },
+  form: {
+    alignSelf: "stretch",
+    paddingHorizontal: 30,
+    marginBottom: 0
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    backgroundColor: "#fff",
+    color: "#444",
+    fontSize: 16,
+    borderRadius: 2,
+    marginBottom: 20
+  },
+  button: {
+    height: 42,
+    backgroundColor: "#DC3545",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2
+  },
+  disabled: {
+    height: 42,
+    backgroundColor: "rgba(220,53,69,.45)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16
+  },
+  logo: {
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center"
+  },
+  subTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#fff",
+    alignSelf: "center",
+    marginBottom: 30
+  }
+});
--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,91 +1,102 @@
-import React from 'react'
-import { Text, View, SafeAreaView, StyleSheet, Platform, TouchableOpacity, Image, TouchableHighlight, AsyncStorage } from 'react-native'
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  Image,
+  TouchableHighlight,
+  AsyncStorage
+} from "react-native";
 
-import logo from '../assets/unikor.png'
+import logo from "../assets/unikor.png";
 
 export default function Report({ navigation }) {
+  const report = navigation.getParam("report");
 
-    const report = navigation.getParam('report')
+  function handleBack() {
+    navigation.navigate("List");
+  }
 
-    function handleBack() {
-        navigation.navigate('List')
-    }
+  async function handleLogout() {
+    await AsyncStorage.removeItem("nome");
+    await AsyncStorage.removeItem("user");
 
-    async function handleLogout() {
-        await AsyncStorage.removeItem('nome')
-        await AsyncStorage.removeItem('user')
+    navigation.navigate("Login");
+  }
 
-        navigation.navigate('Login');
-    }
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableHighlight onPress={handleLogout}>
+        <Image style={styles.logo} source={logo} />
+      </TouchableHighlight>
 
-    return (
-        <SafeAreaView style={styles.container}>
+      <Text style={styles.titleText}>Dados do relatório</Text>
 
-            <TouchableHighlight onPress={handleLogout}>
-                <Image style={styles.logo} source={logo} />
-            </TouchableHighlight>
-
-            <Text style={styles.titleText}>Dados do relatório</Text>
-
-            <View>
-                {report
-                    ? Object.entries(report).filter(i => i[0] !== '_id').map((field, index) => {
-                        return (
-                            <View key={index} style={styles.report}>
-                                <Text style={styles.fieldText}>{field[0]}:</Text>
-                                <Text style={styles.dataText}>{field[1]}</Text>
-                            </View>
-                        );
-                    })
-                    : <Text>Não há dados!</Text>}
-                <TouchableOpacity onPress={handleBack} style={styles.button}>
-                    <Text style={styles.buttonText}>Voltar</Text>
-                </TouchableOpacity>
-            </View>
-
-        </SafeAreaView>
-    );
+      <View>
+        {report ? (
+          Object.entries(report)
+            .filter(i => i[0] !== "_id")
+            .map((field, index) => {
+              return (
+                <View key={index} style={styles.report}>
+                  <Text style={styles.fieldText}>{field[0]}:</Text>
+                  <Text style={styles.dataText}>{field[1]}</Text>
+                </View>
+              );
+            })
+        ) : (
+          <Text>Não há dados!</Text>
+        )}
+        <TouchableOpacity onPress={handleBack} style={styles.button}>
+          <Text style={styles.buttonText}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: Platform.OS === 'android' ? 25 : 0,
-        paddingHorizontal: 20,
-    },
-    titleText: {
-        fontWeight: 'bold',
-        marginTop: 15,
-        fontSize: 24,
-        alignSelf: 'center'
-    },
-    report: {
-        marginTop: 20,
-        alignItems: 'center'
-    },
-    fieldText: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    dataText: {
-        fontSize: 18
-    },
-    button: {
-        height: 42,
-        backgroundColor: '#DC3545',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
-        marginTop: 30,
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
-    logo: {
-        marginTop: 20,
-        height: 60,
-        resizeMode: "contain",
-        alignSelf: "center"
-    }
-})
+  container: {
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+    paddingHorizontal: 20
+  },
+  titleText: {
+    fontWeight: "bold",
+    marginTop: 15,
+    fontSize: 24,
+    alignSelf: "center"
+  },
+  report: {
+    marginTop: 20,
+    alignItems: "center"
+  },
+  fieldText: {
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  dataText: {
+    fontSize: 18
+  },
+  button: {
+    height: 42,
+    backgroundColor: "#DC3545",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+    marginTop: 30
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  logo: {
+    marginTop: 20,
+    height: 60,
+    resizeMode: "contain",
+    alignSelf: "center"
+  }
+});
